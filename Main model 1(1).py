a454997620db1554b181e9a050ebce4432b5bae2
kirubel-Biruk-Shiferaw <84914578+kirubel-Biruk-Shiferaw@users.noreply.github.com>
2023-08-17 13:29:41 +0200

    Main prediction model.py
    This is the python code for developing the main model used in this study. You should use python environment and install the libraries before running the code. Python version 13.9 was used during the development of this model.

--- Main model 1(1).py
+++ Main model 1(1).py
@@ -1,10 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[19]:
-
-
-# Libraries
+# Import necessary Libraries or install if not yet installed
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
@@ -17,36 +14,19 @@ get_ipython().run_line_magic('matplotlib', 'inline')
 import json
 import glob
 
+# set the data paht/location
+data = pd.read_excel("data path/your own location")
 
-# In[20]:
-
-
-data = pd.read_excel("C:/Users/kirubel/Documents/Mule Project/Raw final.xlsx")
-
-
-# In[21]:
-
-
-#we drope 3 variables for very low correlation and high imbalance
+#we droped 3 variables for very low correlation and high imbalance
 data= data.drop('wasting', axis=1)
 data= data.drop('water', axis=1)
 data= data.drop('vitamin', axis=1)
 
-
-# In[22]:
-
-
+# compute the correlation among the variables
 data.corr()
 
-
-# In[23]:
-
-
+#see the correlation heatmap
 sns.heatmap(data.corr(), cmap='inferno') #Very good correlation 
-
-
-# In[24]:
-
 
 corr= data.corr()
 
@@ -56,40 +36,24 @@ matrix = np.triu(np.ones_like(corr))
 # using the upper triangle matrix as mask 
 sns.heatmap(data.corr(), mask=matrix)
 
-
-# In[25]:
-
-
+#Defining the outcome variable and the independent variables/ARI in our case
 X= data.drop(["outcomevar"], axis = 1)
 Y= data["outcomevar"]
-
-
-# In[26]:
-
 
 # Train/Test split
 from sklearn.model_selection import train_test_split
 x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size = 0.2, random_state = 42)
 
-
-# In[27]:
-
-
+#From the resampling comparision file, we can see that neear miss undersampling performed relatively better and we used it to develope the model used in this study
 #Nearmiss undersampling
 from imblearn.under_sampling import NearMiss
 nearmiss = NearMiss()
 X_train_nearmiss, y_train_nearmiss = nearmiss.fit_resample(x_train, y_train)
 
-
-# In[28]:
-
-
+#set the resampled dataset for experiment
 x_train, x_test, y_train, y_test = train_test_split(X_train_nearmiss, y_train_nearmiss, test_size = 0.2, random_state = 42)
 
-
-# In[120]:
-
-
+# see the shape of the resampled dataset
 print("x_train : ",x_train.shape)
 
 print("x_test : ",x_test.shape)
@@ -98,20 +62,13 @@ print("y_train : ",y_train.shape)
 
 print("y_test : ",y_test.shape)
 
-
-# In[122]:
-
-
 # Convert the arrays or DataFrames to DataFrames if needed
 x_train_df = pd.DataFrame(x_train)
 x_test_df = pd.DataFrame(x_test)
 y_train_df = pd.DataFrame(y_train)
 y_test_df = pd.DataFrame(y_test)
 
-
-# In[129]:
-
-
+#save if necessary
 # Save x_train to Excel
 x_train_df.to_excel("C:/Users/kirubel/Documents/MuleProject/Code/Docker/Dockerfile/x_train.xlsx", index=False)
 
@@ -124,39 +81,18 @@ y_train_df.to_excel("C:/Users/kirubel/Documents/MuleProject/Code/Docker/Dockerfi
 # Save y_test to Excel
 y_test_df.to_excel("C:/Users/kirubel/Documents/MuleProject/Code/Docker/Dockerfile/y_test.xlsx", index=False)
 
-
-# In[ ]:
-
-
-
-
-
-# In[29]:
-
-
 # Check the number of records after over sampling
 from collections import Counter
 print(sorted(Counter(y_train_nearmiss).items()))
 
-
-# In[30]:
-
-
+#From the model comparision module, we identified that three models perform better (SVM, GB and XGB)
 #Training the first model, SVM
 #SVM gride search (done) and model training
 from sklearn.svm import SVC
 svc_tuned = SVC(probability=True, C= 1, gamma= 1, kernel= 'linear')
 svc_tuned.fit(x_train, y_train)
 
-
-# In[31]:
-
-
 svc_pred= svc_tuned.predict(x_test)
-
-
-# In[32]:
-
 
 #Training the second model, GB
 from sklearn.ensemble import GradientBoostingClassifier
@@ -166,25 +102,12 @@ gb_tuned.fit(x_train, y_train)
 # Make predictions on the test set
 GBpred = gb_tuned.predict(x_test)
 
-
-# In[33]:
-
-
 #Training the third model, XGB
 #XGB grid search(done) and model training
 from xgboost import XGBClassifier
 xgb_tuned=XGBClassifier(learning_rate= 0.01, max_depth= 3, n_estimators= 200)
 xgb_tuned.fit(x_train, y_train)
-
-
-# In[34]:
-
-
 XGBpred= xgb_tuned.predict(x_test)
-
-
-# In[35]:
-
 
 # Ensemling the models
 # Import required libraries
@@ -197,23 +120,11 @@ ensemble_model = VotingClassifier(estimators=[('svm',svc_tuned ), ('gb', gb_tune
 # Fit the ensemble model to the data
 ensemble_model.fit(x_train, y_train)
 
-
-# In[36]:
-
-
 # Predict the target variable
 y_pred_ensemble = ensemble_model.predict(x_train)
 
-
-# In[37]:
-
-
 from sklearn.metrics import classification_report
 print(classification_report(y_train, y_pred_ensemble))
-
-
-# In[38]:
-
 
 # Ensure the predictions have the same number of samples
 min_samples = min(len(svc_pred), len(GBpred), len(XGBpred))
@@ -222,20 +133,13 @@ xg_predictions = GBpred[:min_samples]
 xgb_predictions = XGBpred[:min_samples]
 y_test = y_test[:min_samples]
 
-
-# In[39]:
-
-
 ensemble_predictions = []
 for i in range(len(y_test)):
     # Majority voting
     votes = svc_pred[i] + GBpred[i] + XGBpred[i]
     ensemble_predictions.append(1 if votes >= 2 else 0)
 
-
-# In[40]:
-
-
+# calculating the metrics
 from sklearn.metrics import confusion_matrix
 from sklearn.metrics import recall_score
 cm = confusion_matrix(y_test, ensemble_predictions)
@@ -248,10 +152,6 @@ specificity = cm[0,0] / (cm[0,0] + cm[0,1])
 # print the results
 print("Sensitivity:", sensitivity)
 print("Specificity:", specificity)
-
-
-# In[41]:
-
 
 # Use learning_curve to generate the training and test accuracy scores
 from sklearn.model_selection import learning_curve, train_test_split
@@ -273,74 +173,32 @@ plt.ylabel("Score")
 plt.legend(loc="best")
 plt.show()
 
-
-# In[42]:
-
-
 # Extract feature importances from the base classifiers
 svm_feature_importances = np.abs(svc_tuned.coef_[0])
 gb_feature_importances = gb_tuned.feature_importances_
 xgb_feature_importances = xgb_tuned.feature_importances_
 
-
-# In[43]:
-
-
 # Calculate average feature importance across the Gradient Boosting and XGBoost classifiers
 ensemble_feature_importances = (gb_feature_importances + xgb_feature_importances + svm_feature_importances) / 3
-
-
-# In[44]:
-
 
 # Sort feature importances in descending order
 sorted_indices = np.argsort(ensemble_feature_importances)[::-1]
 sorted_importances = ensemble_feature_importances[sorted_indices]
 sorted_features = np.arange(X.shape[1])[sorted_indices]
 
-
-# In[82]:
-
-
 sorted_features
 
-
-# In[67]:
-
-
 ensemble_feature_importances
-
-
-# In[46]:
-
 
 # Create a list of feature names
 feature_names = X.columns
 feature_names
 
-
-# In[72]:
-
-
 feature_importances = pd.DataFrame(ensemble_feature_importances, index= feature_names)
 feature_importances.head()
 
-
-# In[48]:
-
-
 features = list(feature_importances [feature_importances[0]>0].index)
 features
-
-
-# In[ ]:
-
-
-
-
-
-# In[76]:
-
 
 # Plot the feature importances
 plt.figure( figsize=(8, 6))
@@ -351,10 +209,6 @@ plt.ylabel('Importance')
 plt.title('Feature Importance')
 plt.show()
 
-
-# In[65]:
-
-
 # Plot the feature importances
 plt.figure(figsize=(8, 6))
 plt.bar(range(X.shape[1]), ensemble_feature_importances, align='center')
@@ -364,27 +218,15 @@ plt.ylabel('Importance')
 plt.title('Feature Importance')
 plt.show()
 
-
-# In[84]:
-
-
+# Plot the SHAP values
 def model_predict(x_test):
     return ensemble_model.predict(x_test)
-
-
-# In[100]:
-
-
 
 import shap
 explainer = shap.Explainer(model_predict, x_test)
 shap_values = explainer(x_test)
 
 shap.summary_plot(shap_values, x_test)
-
-
-# In[98]:
-
 
 #compute shap values
 explainer = shap.Explainer(model_predict, x_test)
@@ -395,16 +237,7 @@ if len(shap_values.shape) == 1:
     shap_values = shap_values.reshape((-1, x_test.shape[1]))
     
 shap.summary_plot(shap_values.data, x_test.values, feature_names = x_test.columns)
-
-
-# In[94]:
-
-
 x_test.shape
-
-
-# In[99]:
-
 
 #compute shap values
 explainer = shap.Explainer(model_predict, x_test)
@@ -416,10 +249,6 @@ if len(shap_values.shape) == 1:
     
 shap.summary_plot(shap_values.data, x_test.values, feature_names = x_test.columns, plot_type = "violin")
 
-
-# In[101]:
-
-
 #compute shap values
 explainer = shap.Explainer(model_predict, x_test)
 shap_values = explainer(x_test)
@@ -429,11 +258,6 @@ if len(shap_values.shape) == 1:
     shap_values = shap_values.reshape((-1, x_test.shape[1]))
     
 shap.summary_plot(shap_values, x_test)
-
-
-
-# In[112]:
-
 
 #compute shap values
 explainer = shap.Explainer(model_predict, x_test)
@@ -445,10 +269,6 @@ if len(shap_values.shape) == 1:
     
 shap.plots.beeswarm(shap_values, plot_type= "violin")
 
-
-# In[114]:
-
-
 #compute shap values
 explainer = shap.Explainer(model_predict, x_test)
 shap_values = explainer(x_test)
@@ -458,10 +278,6 @@ if len(shap_values.shape) == 1:
     shap_values = shap_values.reshape((-1, x_test.shape[1]))
     
 shap.summary_plot(shap_values, plot_type = "violin")
-
-
-# In[ ]:
-
 
 #compute shap values
 explainer = shap.Explainer(model_predict, x_test)
@@ -473,37 +289,18 @@ if len(shap_values.shape) == 1:
     
 shap.summary_plot(shap_values.data, x_test.values, feature_names = x_test.columns, plot_type = "violin")
 
-
-# In[155]:
-
-
+#Export the trained model 
 import os
 print(os.getcwd())
 
-
-# In[154]:
-
-
-os.chdir('C:/Users/kirubel/Documents/MuleProject/Code/app')
-
-
-# In[156]:
-
+os.chdir('your Path/location to save the model')
 
 #serializing the model (a serialized model file refers to a file that stores the trained model object in a serialized format, allowing it to be saved and loaded for later use. It typically contains all the necessary information to recreate the trained model, including the model architecture, weights, and any additional parameters. )
 import pickle
 
-
-# In[157]:
-
-
-model_path = 'C:/Users/kirubel/Documents/MuleProject/Code/app/trained_model.pkl'
+model_path = 'your model path'
 with open(model_path, 'wb') as file:
     pickle.dump(ensemble_model, file)
-
-
-# In[158]:
-
 
 # Save the model to a file
 data = {"model": ensemble_model, "Important features": features}
@@ -512,32 +309,8 @@ with open('trained_ensemble_model.pkl', 'wb') as file:
     pickle.dump(data, file)
 
 
-# In[ ]:
-
-
-
-
-
-# In[141]:
-
-
-from platform import python_version
-
-print(python_version())
-
-
-# In[159]:
-
-
-
-
-# Load the saved model from the file
+# Load the saved model from the file for coherence
 with open('trained_model.pkl', 'rb') as file:
     loaded_model = pickle.load(file)
-
-
-# In[ ]:
-
-
 
 
